cc36806b99c472e9073106b47fe342d909307b64
mmitushevskiMelon <66066473+mmitushevskiMelon@users.noreply.github.com>
2021-12-31 13:12:12 +0100

    Create AdapptrConfig.cs

---
+++ Adapptr/Shareholding Disclosure/C#/AdapptrConfig.cs
@@ -0,0 +1,9 @@
+namespace FundAppsScripts.Scripts
+{
+        public class AdapptrConfig
+        {
+            public string BaseUrl { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+}
